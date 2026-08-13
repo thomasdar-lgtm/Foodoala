06f7e4b07ffffb5fe317dfb1cae31a58da09573d
thomasdar-lgtm <thomas.darregert@gmail.com>
2026-08-13 18:47:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'foodoala-v1.21';
+const CACHE_VERSION = 'foodoala-v1.22';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icons/icon-32.png', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
 
